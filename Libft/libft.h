9d65ad91478d91d3d902263e1bee983a83ca3489
Silvestri Ndoj <silndoj@3-G-5.42heilbronn.de>
2024-03-13 12:15:04 +0100

    Libft/

---
+++ Libft/libft.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: silndoj <silndoj@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/09 12:39:52 by silndoj           #+#    #+#             */
+/*   Updated: 2024/03/12 17:25:02 by silndoj          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t len);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memset(void *str, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+
+#endif
